0ba000ac89aaaec95e1dde3ecb587432ca794f4b
wstlabs <notyet@notyet.org>
2017-06-30 09:06:27 -0400

    refactor into basic working state

--- ezbpg/__main__.py
+++ ezbpg/__main__.py
@@ -10,40 +10,50 @@ def parse_args():
     parser.add_argument("--infile", help="csv file to parse", required=True)
     return parser.parse_args()
 
+def mkdir_soft(dirpath):
+    if not os.path.exists(dirpath):
+        os.mkdir(dirpath)
+
 # edgeseq = purify(csviter(args.infile))
 # g = Matcher(edgeseq)
-g = ezbpg.slurp(args.infile)
-print("Consumed %d edge observations, of which %d were distinct." % (g.observed,g.distinct))
-print("stats = ",json.dumps(g.stats(),sort_keys=True))
+def refine(g):
+    p = partition_forest(g)
+    r = refine_partition(p)
+    rows,total = describe_partition(r)
+    print(tabulate(rows,headers="firstrow"))
 
-p = partition_forest(g)
-r = refine_partition(p)
-rows,total = describe_partition(r)
-print(tabulate(rows,headers="firstrow"))
+    print("Making for %d components total." % total['component'])
+    for tag in sorted(r.keys()):
+        print("class[%s] = %s" % (tag,{x:len(r[tag][x]) for x in r[tag]}))
+    return r
 
-print("Making for %d components total." % total['component'])
-for tag in sorted(r.keys()):
-    print("class[%s] = %s" % (tag,{x:len(r[tag][x]) for x in r[tag]}))
-
-
-tag = 'm-n'
-outdir = "comp/%s" % tag
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
-print("tag '%s' has %d component class(es)." % (tag,len(r[tag])))
-for t in sorted(r[tag].keys()):
-    nj,nk = t
-    components = r[tag][t]
-    print ("class[%s] = has %d component(s):" % (t,len(components)))
-    for i,edgelist in enumerate(components):
-        basefile = "%d,%d-%d.txt" % (nj,nk,i)
-        outpath = "%s/%s" % (outdir,basefile)
-        print("%s .." % outpath)
-        with open(outpath,"wt") as f:
-            save_edges(f,edgelist)
+def extract(outdir,tag,r):
+    mkdir_soft(outdir)
+    subdir = "comp/%s" % tag
+    mkdir_soft(subdir)
+    print("tag '%s' has %d component class(es)." % (tag,len(r[tag])))
+    for t in sorted(r[tag].keys()):
+        nj,nk = t
+        components = r[tag][t]
+        print ("class[%s] = has %d component(s):" % (t,len(components)))
+        for i,edgelist in enumerate(components):
+            basefile = "%d,%d-%d.txt" % (nj,nk,i)
+            outpath = "%s/%s" % (subdir,basefile)
+            print("%s .." % outpath)
+            with open(outpath,"wt") as f:
+                save_edges(f,edgelist)
 
 def main():
     args = parse_args()
+
+    g = ezbpg.slurp(args.infile)
+    print("Consumed %d edge observations, of which %d were distinct." % (g.observed,g.distinct))
+    print("stats = ",json.dumps(g.stats(),sort_keys=True))
+    r = refine(g)
+
+    tag = 'm-n'
+    outdir = 'comp'
+    extract(outdir,tag,r)
 
 if __name__ == '__main__':
     main()
