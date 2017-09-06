783c55d10c9e1be2c76ec99a65d938012e24dcac
wstlabs <notyet@notyet.org>
2017-09-06 16:38:00 -0400

    add check on tuple length

--- ezbpg/ioutil.py
+++ ezbpg/ioutil.py
@@ -11,8 +11,14 @@ def csviter(path,encoding='utf-8'):
         yield tuple(line.rstrip().split(','))
 
 def purify(tupseq):
-    print(tupseq)
-    for x,y in tupseq:
+    for t in tupseq:
+         # We do this check because from time to time we ingest tuples that either come 
+         # from CSV files that are themselves dirty (e.g not strictly 2 columns), or the
+         # CSV parser has been incorrectly configured (to not recognize quote lines with 
+         # embedded commas, for example).
+        if len(t) != 2:
+            raise ValueError("invalid tuple length")
+        x,y = t
         if x is not None and y is not None:
             yield x,y
 
